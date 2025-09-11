8429db940757d5dfd6f05b9dd600e3fd47f09299
Naufal Ilyasa <naufal.ilyasa7@gmail.com>
2025-09-11 22:18:40 +0700

    fix: uncommented at serverless index.ts

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,4 +1,4 @@
-// import serverless from "serverless-http";
-// import app from "./app.js";
+import serverless from "serverless-http";
+import app from "./app.js";
 
-// export const handler = serverless(app);
+export const handler = serverless(app);
